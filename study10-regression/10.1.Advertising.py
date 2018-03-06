538bd9f06ff691bdd567e91226c5eb49808ec91a
yangxueyi <yangxueyi@kingsoft.com>
2018-03-06 08:30:07 +0800

    添加广告投放渠道和效果线性模型代码注释

--- study10-regression/10.1.Advertising.py
+++ study10-regression/10.1.Advertising.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from pprint import pprint
 
-
 if __name__ == "__main__":
     path = '10.Advertising.csv'
     # # 手写读取数据 - 请自行分析，在10.2.Iris代码中给出类似的例子
@@ -24,7 +23,9 @@ if __name__ == "__main__":
     #     if not d:
     #         continue
     #     d = map(float, d.split(','))
+    #     # 读取第一列到最后一列不包含最后一列
     #     x.append(d[1:-1])
+    #     # 读取最后一列
     #     y.append(d[-1])
     # pprint(x)
     # pprint(y)
@@ -40,18 +41,21 @@ if __name__ == "__main__":
     # f.close()
 
     # # numpy读入
+    # skiprows=1 跳过第一行的标题
     # p = np.loadtxt(path, delimiter=',', skiprows=1)
     # print p
     # print '\n\n===============\n\n'
 
     # pandas读入
-    data = pd.read_csv(path)    # TV、Radio、Newspaper、Sales
+    data = pd.read_csv(path)  # TV、Radio、Newspaper、Sales
     # x = data[['TV', 'Radio', 'Newspaper']]
+    # newspaper观察并没有一个线性关系，不考虑
     x = data[['TV', 'Radio']]
     y = data['Sales']
-    print x
-    print y
+    # print x
+    # print y
 
+    # 绘图显示中文
     mpl.rcParams['font.sans-serif'] = [u'simHei']
     mpl.rcParams['axes.unicode_minus'] = False
 
@@ -64,15 +68,20 @@ if __name__ == "__main__":
     # plt.show()
     # #
     # 绘制2
-    plt.figure(figsize=(9,12))
+    # figsize指定宽高
+    plt.figure(figsize=(9, 12))
+    # 三行一列 第一个子图
     plt.subplot(311)
+    # tv列和y价格对应的关系
     plt.plot(data['TV'], y, 'ro')
     plt.title('TV')
     plt.grid()
+    # 三行一列 第二个子图
     plt.subplot(312)
     plt.plot(data['Radio'], y, 'g^')
     plt.title('Radio')
     plt.grid()
+    # 三行三列 第三个子图
     plt.subplot(313)
     plt.plot(data['Newspaper'], y, 'b*')
     plt.title('Newspaper')
@@ -80,19 +89,25 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.show()
 
+    # train_test_split 百分之80用来训练，百分之20用来测试，得到 x_train, x_test   和  y_train, y_test
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
     # print x_train, y_train
+    # 线性回归
     linreg = LinearRegression()
+    # 用训练数据训练模型
     model = linreg.fit(x_train, y_train)
     print model
     print linreg.coef_
     print linreg.intercept_
 
-    y_hat = linreg.predict(np.array(x_test))
+    # 用测试数据预测，产生yhat
+    y_hat = linreg.predict(x_test)
+    # 均方误差
     mse = np.average((y_hat - np.array(y_test)) ** 2)  # Mean Squared Error
     rmse = np.sqrt(mse)  # Root Mean Squared Error
     print mse, rmse
 
+    # 绘制测试数据和预测数据曲线
     t = np.arange(len(x_test))
     plt.plot(t, y_test, 'r-', linewidth=2, label=u'真实数据')
     plt.plot(t, y_hat, 'g-', linewidth=2, label=u'预测数据')
